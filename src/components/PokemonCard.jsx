2f1a1a32599adc183ac737c5a8ab6a4e0c917082
JoYoungHyun <younghyun550@gmail.com>
2025-02-03 18:00:00 +0900

    💄 Feat: 리스트 UI 구현 및 PokemonCard 렌더링구현 (#4)

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,60 @@
-const PokemonCard = () => {
-  return <div>PokemonCard</div>;
+import styled from "styled-components";
+
+const PokemonCard = ({ pokemon }) => {
+  return (
+    <Card key={pokemon.id}>
+      <img src={pokemon.img_url} />
+      <div>
+        <p className="pokemonName">{pokemon.korean_name}</p>
+        <p className="pokemonNo">No. {"00" + pokemon.id}</p>
+      </div>
+      <button>추가</button>
+    </Card>
+  );
 };
+
+const Card = styled.div`
+  border: 1px solid rgb(221, 221, 221);
+  background-color: rgb(255, 255, 255);
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 8px;
+  text-align: center;
+  padding: 10px;
+  cursor: pointer;
+  transition: transform 0.2s, box-shadow 0.2s;
+
+  img {
+    width: 100px;
+    height: 100px;
+  }
+
+  div {
+    margin-top: 10px;
+
+    .pokemonName {
+      font-size: 14px;
+      font-weight: bold;
+      margin: 5px 0px;
+      color: black;
+    }
+
+    .pokemonNo {
+      font-size: 12px;
+      color: rgb(102, 102, 102);
+    }
+  }
+
+  button {
+    margin-top: 10px;
+    padding: 5px 10px;
+    font-size: 12px;
+    cursor: pointer;
+    border: none;
+    background-color: rgb(255, 0, 0);
+    color: rgb(255, 255, 255);
+    border-radius: 5px;
+  }
+`;
 
 export default PokemonCard;
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import MOCK_DATA from "../data/mokadata";
 import { useState } from "react";
+import PokemonCard from "./PokemonCard";
 
 const PokemonList = () => {
   const [pokemonList, setPokemonList] = useState([...MOCK_DATA]);
@@ -9,16 +10,7 @@ const PokemonList = () => {
   return (
     <ListDiv>
       {pokemonList.map((pokemon) => {
-        return (
-          <div className="card" key={pokemon.id}>
-            <img src={pokemon.img_url} />
-            <div>
-              <p className="pokemonName">{pokemon.korean_name}</p>
-              <p className="pokemonNo">No. {"00" + pokemon.id}</p>
-            </div>
-            <button>추가</button>
-          </div>
-        );
+        return <PokemonCard key={pokemon.id} pokemon={pokemon} />;
       })}
     </ListDiv>
   );
@@ -32,50 +24,6 @@ const ListDiv = styled.div`
   padding: 20px;
   border: 1px solid rgb(221, 221, 221);
   border-radius: 10px;
-
-  .card {
-    border: 1px solid rgb(221, 221, 221);
-    background-color: rgb(255, 255, 255);
-    border-radius: 10px;
-    overflow: hidden;
-    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 8px;
-    text-align: center;
-    padding: 10px;
-    cursor: pointer;
-    transition: transform 0.2s, box-shadow 0.2s;
-
-    img {
-      width: 100px;
-      height: 100px;
-    }
-
-    div {
-      margin-top: 10px;
-
-      .pokemonName {
-        font-size: 14px;
-        font-weight: bold;
-        margin: 5px 0px;
-        color: black;
-      }
-
-      .pokemonNo {
-        font-size: 12px;
-        color: rgb(102, 102, 102);
-      }
-    }
-
-    button {
-      margin-top: 10px;
-      padding: 5px 10px;
-      font-size: 12px;
-      cursor: pointer;
-      border: none;
-      background-color: rgb(255, 0, 0);
-      color: rgb(255, 255, 255);
-      border-radius: 5px;
-    }
-  }
 `;
 
 export default PokemonList;
